6a18c26774a896e36aa4a815b1060576d0f27fab
Haye Böhm <haye.bohm@gmail.com>
2015-03-12 09:33:35 +0100

    Added skeleton for assignment 6. Also: added files from Blackboard.

--- Assignment6/src/assignment6/Assignment6.java
+++ Assignment6/src/assignment6/Assignment6.java
@@ -1,9 +1,14 @@
 package assignment6;
 
+/**
+ * 
+ * @author Haye Bohm - s4290402
+ * @author Ylja Remmits - s4373510
+ */
 public class Assignment6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
