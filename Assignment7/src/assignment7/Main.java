5dfefde4a8271b218b916cb98e0a402f98556924
Haye Böhm <haye.bohm@gmail.com>
2015-03-17 14:22:47 +0100

    Added files from blackboard.

--- Assignment7/src/assignment7/Main.java
+++ Assignment7/src/assignment7/Main.java
@@ -1,10 +1,22 @@
 package assignment7;
 
+
+/**
+ *
+ * @author Sjaak Smetsers
+ */
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Polynomial p1 = new Polynomial( "3.0 1 2 3" );
+        Polynomial p2 = new Polynomial( "4 0 5 3" );
+        Polynomial p3 = new Polynomial( "4 0 -5 3" );
+        Polynomial p4 = new Polynomial( "6 1" );
 
-	}
+        System.out.println( p1 );
+        System.out.println( p2 );
+        System.out.println( p3 );
+        System.out.println( p4 );
+   }
 
 }
